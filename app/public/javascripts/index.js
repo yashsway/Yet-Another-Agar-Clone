d64940269aea9a99988d5bf7bb7f0c0ac13761b9
JemarJones <jemarkjones@gmail.com>
2015-10-25 14:29:04 -0400

    Got movement over the server partially working

--- app/public/javascripts/index.js
+++ app/public/javascripts/index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var socket = io();
 	var canvas = document.getElementById("agarCanvas");
 	var context = canvas.getContext("2d");
-	var mouse= {x:0, y:0};
+	var mouse = {x:0, y:0};
 	var pls;
 	var player;
 
@@ -47,25 +47,27 @@ $(document).ready(function(){
 
 	function getDirection(e){
 		var nextDir = direction();
-		console.log(nextDir);
-		socket.emit('objUpdate',{id: player.id, x: player.x, y: player.y, mass: player.mass, color: player.color, direction: nextDir});
+		var newObj = {id: player.id, x: player.x, y: player.y, mass: player.mass, color: player.color, direction: nextDir};
+		socket.emit('objUpdate',newObj);
 	}
-	
+
 	function drawPlayers(players){
+		context.clearRect(0,0,canvas.width,canvas.height);
 		for (i = 0; i < players.length ; i++){
 			drawCircle(players[i].x, players[i].y, players[i].mass, players[i].color);
 		}
 	}
 	socket.on('ready',function(response){
 		pls = response[0];
-		player = response[1];
+		player = response[0][response[1]];
 		drawPlayers(pls);
 		window.addEventListener('mousemove', mousePos);
-		window.addEventListener('mousemove', getDirection);
+		// window.addEventListener('mousemove', getDirection);
 	});
 
 	socket.on('update',function(response){
 		pls = response;
 		drawPlayers(pls);
+		getDirection();
 	});
 });
