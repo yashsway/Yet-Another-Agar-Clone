ccaaf5370518ad0b25f5133d6d358fd395cb0052
JemarJones <jemarkjones@gmail.com>
2015-10-25 13:50:03 -0400

    Connected front-end to backend with socket.io to facilitate movement

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,17 +6,18 @@ module.exports.getRouter = function(io){
 	router.get('/', function(req, res, next) {
 		res.render('index.html');
 	});
-	
+
 	var blobs = [];
 	var blobCount = 0;
 
 	io.on('connection', function(socket){
 		var newId = blobs.length;
 		blobs[newId] = {id: blobCount++, x: Math.floor((Math.random() * 1200) + 0), y: Math.floor((Math.random() * 600) + 0), mass: 20, color: "green"};
-		socket.emit(blobs[newId]);
+		var response = [blobs, newId];
+		socket.emit('ready',response);
 		console.log('Blob ' + newId + " Connected");
 		socket.on('objUpdate',function(obj){
-			for {var i = 0; i <= blobs.length; i++}{
+			for (var i = 0; i <= blobs.length; i++){
 				if (blobs[i].id == obj.id){
 					switch(obj.direction){
 						case "up":
