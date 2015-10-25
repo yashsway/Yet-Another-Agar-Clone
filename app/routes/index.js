8e1698728f1ec466bfd775382b36f60f5b26ddbc
JemarJones <jemarkjones@gmail.com>
2015-10-25 13:23:01 -0400

    Added some backend logic for updating blob position

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,12 +6,62 @@ module.exports.getRouter = function(io){
 	router.get('/', function(req, res, next) {
 		res.render('index.html');
 	});
+	
+	var blobs = [];
+	var blobCount = 0;
 
 	io.on('connection', function(socket){
-		console.log('a user connected');
+		var newId = blobs.length;
+		blobs[newId] = {id: blobCount++, x: Math.floor((Math.random() * 1200) + 0), y: Math.floor((Math.random() * 600) + 0), mass: 20, color: "green"};
+		socket.emit(blobs[newId]);
+		console.log('Blob ' + newId + " Connected");
+		socket.on('objUpdate',function(obj){
+			for {var i = 0; i <= blobs.length; i++}{
+				if (blobs[i].id == obj.id){
+					switch(obj.direction){
+						case "up":
+							blobs[i].y--;
+							break;
+						case "up-right":
+							blobs[i].y--;
+							blobs[i].x++;
+							break;
+						case "up-left":
+							blobs[i].y--;
+							blobs[i].x--;
+							break;
+						case "down":
+							blobs[i].y++;
+							break;
+						case "down-left":
+							blobs[i].y++;
+							blobs[i].x--;
+							break;
+						case "down-right":
+							blobs[i].y++;
+							blobs[i].x++;
+							break;
+						case "right":
+							blobs[i].x++;
+							break;
+						case "left":
+							blobs[i].x--;
+							break;
+						default:
+							break;
+					}
+				}
+				break;
+			}
+		});
 	});
+	//This is our 'game loop' implemented as a callback loop.
+	var sendBlobs = function(){
+		io.emit('update',blobs);
+		setTimeout(sendBlobs,5);
+	};
+	sendBlobs();
 
 	return router;
 };
 
-//module.exports = router;
